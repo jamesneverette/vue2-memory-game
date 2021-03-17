5cbee0d26c3e1d86729eca4d20b602a781dc6a45
James Neverette <jimmyneverette@gmail.com>
2021-03-17 19:30:15 -0400

    Remove not needed prefixes

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -36,18 +36,7 @@ export default {
 
 		&-front,
 		&-back {
-			-webkit-backface-visibility: hidden;
-			-moz-backface-visibility: hidden;
-			-o-backface-visibility: hidden;
 			backface-visibility: hidden;
-			-webkit-transition: 0.6s;
-			-webkit-transform-style: preserve-3d;
-			-moz-transition: 0.6s;
-			-moz-transform-style: preserve-3d;
-			-o-transition: 0.6s;
-			-o-transform-style: preserve-3d;
-			-ms-transition: 0.6s;
-			-ms-transform-style: preserve-3d;
 			transition: 0.6s;
 			transform-style: preserve-3d;
 			top: 0;
@@ -65,9 +54,6 @@ export default {
 			);
 		}
 		&-back {
-			-webkit-transform: rotateY(-180deg);
-			-moz-transform: rotateY(-180deg);
-			-o-transform: rotateY(-180deg);
 			-ms-transform: rotateY(-180deg);
 			transform: rotateY(-180deg);
 			position: absolute;
@@ -77,17 +63,9 @@ export default {
 	&.is-toggled {
 		.c-card__inner {
 			&-front {
-				-webkit-transform: rotateY(180deg);
-				-moz-transform: rotateY(180deg);
-				-o-transform: rotateY(180deg);
-				-ms-transform: rotateY(180deg);
 				transform: rotateY(180deg);
 			}
 			&-back {
-				-webkit-transform: rotateY(0deg);
-				-moz-transform: rotateY(0deg);
-				-o-transform: rotateY(0deg);
-				-ms-transform: rotateY(0deg);
 				transform: rotateY(0deg);
 			}
 		}
