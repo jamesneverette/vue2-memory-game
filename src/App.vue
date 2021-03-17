a66ad5b67997a57a6d56a2a4dd6b11a9efaae0f1
James Neverette <jimmyneverette@gmail.com>
2021-03-17 19:22:28 -0400

    Start setting up Card component and data

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,94 @@
 <template>
-	<div id="app"></div>
+	<div id="app">
+		<section class="s-board container">
+			<ul
+				class="component c-cards c-cards--list d-flex justify-content-center align-items-center flex-wrap"
+			>
+				<li
+					class="c-cards__item p-1"
+					v-for="(card, index) in cards"
+					:key="index"
+				>
+					<card :card="card" />
+				</li>
+			</ul>
+		</section>
+	</div>
 </template>
 
 <script>
 import Card from '@/components/Card';
 export default {
 	name: 'App',
+
+	components: {
+		Card,
+	},
+
+	data: () => ({
+		cards: [
+			{
+				name: 'Orange',
+				color: 'orange',
+				toggled: false,
+			},
+			{
+				name: 'Red',
+				color: 'red',
+				toggled: false,
+			},
+			{
+				name: 'Blue',
+				color: 'blue',
+				toggled: false,
+			},
+			{
+				name: 'Green',
+				color: 'green',
+				toggled: false,
+			},
+			{
+				name: 'Yellow',
+				color: 'yellow',
+				toggled: false,
+			},
+			{
+				name: 'Purple',
+				color: 'purple',
+				toggled: false,
+			},
+			{
+				name: 'Grey',
+				color: 'grey',
+				toggled: false,
+			},
+			{
+				name: 'Black',
+				color: 'black',
+				toggled: false,
+			},
+			{
+				name: 'Teal',
+				color: 'teal',
+				toggled: false,
+			},
+			{
+				name: 'Pink',
+				color: 'pink',
+				toggled: false,
+			},
+			{
+				name: 'Magenta',
+				color: 'magenta',
+				toggled: false,
+			},
+			{
+				name: 'Brown',
+				color: 'brown',
+				toggled: false,
+			},
+		],
+	}),
 };
 </script>
 
@@ -17,5 +100,16 @@ export default {
 	text-align: center;
 	color: #2c3e50;
 	margin-top: 60px;
+}
+
+.c-cards {
+	&--list {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+	&__item {
+		flex: 0 1 20%;
+	}
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,90 @@
+<template functional>
+	<div class="component c-card">
+		<div class="c-card__inner position-relative">
+			<div class="c-card__inner-front position-absolute"></div>
+			<div
+				class="c-card__inner-back position-absolute"
+				:style="{ backgroundColor: props.card.color }"
+			></div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Card',
+
+	props: {
+		card: {
+			required: true,
+			type: Object,
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.c-card {
+	cursor: pointer;
+	height: 140px;
+	perspective: 1000;
+	width: 100%;
+
+	&__inner {
+		height: 100%;
+		width: 100%;
+
+		&-front,
+		&-back {
+			-webkit-backface-visibility: hidden;
+			-moz-backface-visibility: hidden;
+			-o-backface-visibility: hidden;
+			backface-visibility: hidden;
+			-webkit-transition: 0.6s;
+			-webkit-transform-style: preserve-3d;
+			-moz-transition: 0.6s;
+			-moz-transform-style: preserve-3d;
+			-o-transition: 0.6s;
+			-o-transform-style: preserve-3d;
+			-ms-transition: 0.6s;
+			-ms-transform-style: preserve-3d;
+			transition: 0.6s;
+			transform-style: preserve-3d;
+			top: 0;
+			left: 0;
+			height: 100%;
+			width: 100%;
+		}
+		&-front {
+			background: black;
+		}
+		&-back {
+			-webkit-transform: rotateY(-180deg);
+			-moz-transform: rotateY(-180deg);
+			-o-transform: rotateY(-180deg);
+			-ms-transform: rotateY(-180deg);
+			transform: rotateY(-180deg);
+			position: absolute;
+		}
+	}
+
+	&.is-toggled {
+		&__inner {
+			&-front {
+				-webkit-transform: rotateY(180deg);
+				-moz-transform: rotateY(180deg);
+				-o-transform: rotateY(180deg);
+				-ms-transform: rotateY(180deg);
+				transform: rotateY(180deg);
+			}
+			&-back {
+				-webkit-transform: rotateY(0deg);
+				-moz-transform: rotateY(0deg);
+				-o-transform: rotateY(0deg);
+				-ms-transform: rotateY(0deg);
+				transform: rotateY(0deg);
+			}
+		}
+	}
+}
+</style>
